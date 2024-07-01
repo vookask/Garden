0ba4e84ba7973accde637a04e1ffaf08101ab4b1
Владимир <135458288+vookask@users.noreply.github.com>
2024-07-01 21:48:37 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:vookask/garden.git master:gh-pages
 
 cd -
